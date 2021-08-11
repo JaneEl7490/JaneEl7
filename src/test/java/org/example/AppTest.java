9e4bd5051c592939521b38587722616b162621ff
JaneEl7490 <yami7490@163.com>
2021-08-11 17:17:09 +0800

    first commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("执行 App 类 addNumber()方法");
+        int n1 = 10;
+        int n2 = 20;
+        int res = 0;
+        App app = new App();
+        res = app.add(n1,n2);
+        //期望值，实际值
+        Assert.assertEquals(  30,res);
+    }
+}
